222ebc1d75a2ee371451385f6e2ccb652cc72f32
Jaiveer Chadda <jaiveer.chadda@gmail.com>
2024-03-03 12:18:06 +0000

    Made colour calculation much more efficient

--- source/Meta/PrintColour.py
+++ source/Meta/PrintColour.py
@@ -1,20 +1,28 @@
-from typing import Literal
+color_cache = {}
 
 
 def _rgb_to_256colour(rgb: tuple[int, int, int]) -> int:
-    # Quantize RGB values to fit within the ANSI 256-colour space
-    quantised_rgb: tuple[int, int, int] = tuple[int, int, int]([round((c / 255) * 5) for c in rgb])
-    # Calculate the ANSI 256-colour index
-    return 16 + (quantised_rgb[0] * 36) + (quantised_rgb[1] * 6) + quantised_rgb[2]
+    rgb = tuple(map(int, rgb))
+
+    if rgb in color_cache:
+        return color_cache[rgb]
+
+    r, g, b = rgb
+    result = 16 + ((r * 5) // 256 * 36) + ((g * 5) // 256 * 6) + (b * 5) // 256
+    color_cache[rgb] = result
+    return result
 
 
 def colours_equal(colour1: tuple[int, int, int], colour2: tuple[int, int, int]) -> bool:
     return _rgb_to_256colour(colour1) == _rgb_to_256colour(colour2)
 
 
-def get_colour_setter(colour: tuple[int, int, int], target: Literal['text', 'background'] = 'text') -> str:
-    esc_char: int = 4 if target == 'background' else 3
-    return f"\033[{esc_char}8;5;{_rgb_to_256colour(colour)}m"
+def get_text_colour_setter(colour: tuple[int, int, int]) -> str:
+    return f"\033[38;5;{_rgb_to_256colour(colour)}m"
+
+
+def get_bg_colour_setter(colour: tuple[int, int, int]) -> str:
+    return f"\033[48;5;{_rgb_to_256colour(colour)}m"
 
 
 def get_colour_resetter() -> str:
@@ -24,8 +32,8 @@ def get_colour_resetter() -> str:
 def get_coloured_string(text: str,
                         text_colour: tuple[int, int, int] | None = None,
                         background_colour: tuple[int, int, int] | None = None) -> str:
-    set_text_colour: str = get_colour_setter(text_colour) if text_colour is not None else ""
-    set_bg_colour: str = get_colour_setter(background_colour, 'background') if background_colour is not None else ""
+    set_text_colour: str = get_text_colour_setter(text_colour) if text_colour is not None else ""
+    set_bg_colour: str = get_bg_colour_setter(background_colour) if background_colour is not None else ""
     return f"{set_text_colour}{set_bg_colour}{text}{get_colour_resetter()}"
 
 
@@ -37,6 +45,6 @@ def printc(text: str,
 
 if __name__ == "__main__":
     printc("test")
-    printc("test", (255, 0, 0))
-    printc("test", background_colour=(255, 0, 0))
-    printc("test", (255, 0, 0), (0, 0, 255))
+    printc("test", (255,   0, 0))
+    printc("test", background_colour=(255,   0, 0))
+    printc("test", (255,   0,   0), (  0,   0, 255))
