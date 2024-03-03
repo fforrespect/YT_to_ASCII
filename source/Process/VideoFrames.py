ca496ef5a27ee6df7a464c1ecddbf1d051c0b376
Jaiveer Chadda <jaiveer.chadda@gmail.com>
2024-03-03 09:57:42 +0000

    Added progress bar to frame download

--- source/Process/VideoFrames.py
+++ source/Process/VideoFrames.py
@@ -1,13 +1,12 @@
 from os import listdir, remove
 from glob import glob
 from cv2 import VideoCapture, imwrite, CAP_PROP_FPS, CAP_PROP_FRAME_COUNT, Mat
+from progress.bar import Bar
 
 from Meta import GlobalVars
 
 
 def download(from_path: str, to_path: str) -> None:
-    print("Downloading frames...")
-
     files: list[str] = glob(f"{to_path}*")
     for f in files:
         remove(f)
@@ -18,6 +17,12 @@ def download(from_path: str, to_path: str) -> None:
     GlobalVars.fps = vid_cap.get(CAP_PROP_FPS)
     GlobalVars.new_fps = GlobalVars.fps / GlobalVars.skip
     total_frames: int = int(vid_cap.get(CAP_PROP_FRAME_COUNT))
+
+    bar = Bar(
+        'Downloading Frames',
+        max=total_frames//GlobalVars.skip,
+        suffix='%(index)d/%(max)d - %(percent).1f%% - %(eta)ds'
+    )
 
     for frame_number in range(total_frames):
         success: bool
@@ -31,12 +36,14 @@ def download(from_path: str, to_path: str) -> None:
             break
 
         imwrite(f"{to_path}frame{int(frame_number/GlobalVars.skip)}{GlobalVars.frame_ext}", image)
+        bar.next()
 
     # Release the video capture object
     vid_cap.release()
 
-    print("All frames created")
-    print("FPS:", GlobalVars.fps)
+    bar.finish()
+
+    print(len(listdir(GlobalVars.frames_fp))-1, "frames created")
+    print("Original FPS:", GlobalVars.fps)
     print("Adjusted FPS:", GlobalVars.new_fps)
-    print("Total frames:", len(listdir(GlobalVars.frames_fp))-1)
     print()
