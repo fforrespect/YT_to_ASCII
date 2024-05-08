f961fb9ce07bece94fc23c5cf5794228598fd6bd
Jaiveer Chadda <jaiveer.chadda@gmail.com>
2024-05-08 22:37:16 +0100

    Added non-compressed option and reformatted file

--- source/Process/AsciiArt.py
+++ source/Process/AsciiArt.py
@@ -6,10 +6,14 @@ from Meta.ColourTools import get_bg_colour_setter, get_colour_resetter, colours_
 greys_ = (' ', '.', '-', '"', 'r', '/', '>', ')', '[', 'I', 'Y', 'Z', 'h', '#', '8', '@')
 
 reset: str = get_colour_resetter()
-fill_char: str = " "*2
+fill_char: str = " " * 2
 
 
-def image_to_string(file_path: str, in_colour: bool = True, greys: list[str] = greys_) -> str:
+def image_to_string(
+        file_path: str,
+        in_colour: bool = True,
+        greys: list[str] = greys_,
+        compressed: bool = True) -> str:
     """
     Convert an image to ASCII art.
 
@@ -19,6 +23,8 @@ def image_to_string(file_path: str, in_colour: bool = True, greys: list[str] = g
     :type in_colour: bool
     :param greys: A tuple of characters representing greyscale values.
     :type greys: tuple
+    :param compressed: Should the coloured output be compressed?
+    :type compressed: bool
 
     :returns ASCII art representation of the image
     :rtype str
@@ -26,30 +32,29 @@ def image_to_string(file_path: str, in_colour: bool = True, greys: list[str] = g
 
     image: Image.Image = Image.open(file_path)
     image_array: ndarray = array(image)
-
+    
     if in_colour:
-        out: str = "\n".join([
+        output = "\n".join([
             "".join([
                 (
                     "".join([get_bg_colour_setter(pixel_colour), fill_char])
-                    if current_colour is None or not colours_equal(pixel_colour, current_colour)
+                    if not compressed and
+                       (current_colour is None or not colours_equal(pixel_colour, current_colour))
                     else fill_char
-                )
-                for pixel_colour, current_colour in zip(row, [None] + list(row[:-1]))
-            ])
-            for row in image_array
+                ) for pixel_colour, current_colour in zip(row, [None] + list(row[:-1]))
+            ]) for row in image_array
         ])
-
-        return out
-
+    
     else:
         greyscale: bool = len(image_array.shape) == 2
-
+        
         colour_scale: int = len(greys) - 1
         normalised_image: ndarray = (image_array.mean(axis=2) / 255 * colour_scale if not greyscale
                                      else image_array / 255 * colour_scale).astype(int)
-
-        return "\n".join("".join(greys[val] * 2 for val in row) for row in normalised_image)
+        
+        output = "\n".join("".join(greys[val] * 2 for val in row) for row in normalised_image)
+    
+    return output
 
 
 if __name__ == "__main__":
